9658efe783318eeeb406266db1765a87772fc836
Poom <poom.yoosorn@gmail.com>
2024-09-16 22:59:56 +0700

    Write a Python Service Client - no OOP

--- ros2_ws/src/my_py_pkg/setup.py
+++ ros2_ws/src/my_py_pkg/setup.py
@@ -24,7 +24,8 @@ setup(
             "robot_news_station = my_py_pkg.robot_news_station:main",
             "smartphone = my_py_pkg.smartphone:main",
             "Activity002_number_counter = my_py_pkg.Activity002_number_counter:main",
-            "add_two_ints_server = my_py_pkg.add_two_ints_server:main"
+            "add_two_ints_server = my_py_pkg.add_two_ints_server:main",
+            "add_two_ints_client_no_oop = my_py_pkg.add_two_ints_client_no_oop:main"
         ],
     },
 )
