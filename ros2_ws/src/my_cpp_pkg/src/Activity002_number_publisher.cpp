e20220a94ad6a2df56bd330e6e6d1a238e22d725
Poom <poom.yoosorn@gmail.com>
2024-09-23 22:46:38 +0700

    Get Parameters from a C++

--- ros2_ws/src/my_cpp_pkg/src/Activity002_number_publisher.cpp
+++ ros2_ws/src/my_cpp_pkg/src/Activity002_number_publisher.cpp
@@ -5,12 +5,16 @@
 class Activity002NumberPublisher : public rclcpp::Node 
 {
 public:
-    Activity002NumberPublisher() : Node("Activity002_number_publisher") 
+    Activity002NumberPublisher() : Node("Activity002_number_publisher")
     {
-        this->declare_parameter("name",10);
+        this->declare_parameter("In_number",2);
+        this->declare_parameter("In_publish_freq",1.0);
+
+        number_ = this->get_parameter("In_number").as_int();
+        publish_frequency_ = this->get_parameter("In_publish_freq").as_double();
 
         publisher_ = this->create_publisher<example_interfaces::msg::Int64>("number",10);
-        Timer_ = this->create_wall_timer(std::chrono::milliseconds(500),
+        Timer_ = this->create_wall_timer(std::chrono::milliseconds((int)(1000.0/publish_frequency_)),
                                          std::bind(&Activity002NumberPublisher::publishNumber, this));
         
         RCLCPP_INFO(this->get_logger(), "Activity002_number_publisher has been started");
@@ -20,16 +24,17 @@ private:
     void publishNumber()
     {
         auto msg = example_interfaces::msg::Int64();
-        msg.data = 999;
+        msg.data = number_;
         publisher_->publish(msg);
 
         RCLCPP_INFO(this->get_logger(), "Activity002_number_publisher has been published");
     }
 
 
-
-rclcpp::Publisher<example_interfaces::msg::Int64>::SharedPtr   publisher_;
-rclcpp::TimerBase::SharedPtr Timer_;
+    int number_;
+    double publish_frequency_;
+    rclcpp::Publisher<example_interfaces::msg::Int64>::SharedPtr   publisher_;
+    rclcpp::TimerBase::SharedPtr Timer_;
 
 
 };
