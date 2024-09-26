c73951668dee8a0645ec8d46ae043ed8a18760d9
Poom <poom.yoosorn@gmail.com>
2024-09-26 21:24:22 +0700

    Configure Your Nodes in a Launch File

--- ros2_ws/src/my_robot_bringup/launch/number_app.launch.py
+++ ros2_ws/src/my_robot_bringup/launch/number_app.launch.py
@@ -5,14 +5,30 @@ from launch_ros.actions import Node
 def generate_launch_description():
     ld = LaunchDescription()
     
+    remap_number_topic = ("number","Poom_number")
+    
     number_publisher_node = Node(
         package="my_py_pkg",
-        executable="Activity002_number_publisher"
+        executable="Activity002_number_publisher",
+        name="Poom_number_publisher",
+        remappings=[
+            remap_number_topic
+        ],
+        parameters=[
+            {"In_number":4},
+            {"In_publish_freq" : 0.5}
+            
+        ]
     )
     
     number_counter_node = Node(
         package="my_py_pkg",
-        executable="Activity002_number_counter"
+        executable="Activity002_number_counter",
+        name="Poom_number_counter",
+          remappings=[
+            remap_number_topic,
+            ("number_count","Poom_number_count")
+        ]
     )
     
     
